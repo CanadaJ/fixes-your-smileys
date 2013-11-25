53cad4d984504f80d9469fe439cebdd3ed7e61ef
Justin <justin@DerpyDingo.(none)>
2013-11-25 15:55:19 -0800

    Changed some stuff with smiley.js, now working as intended

--- smileys.js
+++ smileys.js
@@ -1,10 +1,12 @@
-var Twit = require('twit')
-
-var config = require('config1.js');
-
+var Twit = require('twit');
+var config = require('./config1');
 var T = new Twit(config);
 
+var interval = process.argv.splice(2);
+
+console.log('Running...');
 setInterval(function() {
+  console.log('Getting tweets...');
   T.get('statuses/home_timeline', { }, function (err, reply) {
     if(err) return handleError(err)    
     var count = reply.length;
@@ -12,8 +14,7 @@ setInterval(function() {
       var username = reply[i].user.screen_name;      
       var text = reply[i].text;
       var id_str = reply[i].id_str;
-      //console.log(username + '\n' + text + '\n');
-      //continue;
+      console.log(username + '\n' + text + '\n');
       if(username === 'FixesSmileys') continue;
       if(text.indexOf(':)') !== -1) {
         var status = '@' + username + ' "' + text + '" \n\n*:^) fixed that for you'; 
@@ -24,7 +25,7 @@ setInterval(function() {
       }
     }	
   })
-}, 120000);
+}, parseInt(interval));
 
 function handleError(err) {
   console.error('response status:', err.statusCode);
